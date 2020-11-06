21f9b354eaa42fa1496ddf5cf78ea98419d3ba92
georgejeswin <jeswinmyladoor@gmail.com>
2020-11-06 08:31:59 +0530

    NotFound page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import {BrowserRouter as Router,Route} from 'react-router-dom';
-import {Container} from 'react-bootstrap'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-// import ErrorPage from './components/Error'
-
-const App=()=> {
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import NotFound from "./components/NotFound";
+const App = () => {
   return (
     <Router>
-    <Header/>
-     <main className="py-2">
-     <Container>
-     <Route path="/" component={HomeScreen} exact/> 
-     <Route path="/product/:id" component={ProductScreen} />
-     </Container>
-     
-     </main>
-     <Footer/>
-
+      <Header />
+      <main className="py-2">
+        <Container>
+          <Switch>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route component={NotFound} />
+          </Switch>
+        </Container>
+      </main>
+      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
