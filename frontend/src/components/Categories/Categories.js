534e9a187ad272d386189dcd90d899628355ee6a
georgejeswin <jeswinmyladoor@gmail.com>
2020-11-08 00:25:36 +0530

    catergory icons

--- frontend/src/components/Categories/Categories.js
+++ frontend/src/components/Categories/Categories.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import "./Categories.css";
 import { LinkContainer } from "react-router-bootstrap";
+import Properties from "./CategoryImages/properties-logo.png";
+import Vehicles from "./CategoryImages/vehicles-logo.png";
+import Furnitures from "./CategoryImages/furniture-logo.png";
+import Applicences from "./CategoryImages/appliances-logo.png";
+import Electronics from "./CategoryImages/electronics-logo.png";
+import Sports from "./CategoryImages/books-and-spots.png";
+
 
 const Categories = () => {
   return (
@@ -10,37 +17,37 @@ const Categories = () => {
         <div className="row">
           <LinkContainer to="/properties">
             <div className="col-md-2 col-4 text-center categoryItems pt-3 pb-3 w-80">
-              <i className="far fa-building fa-2x categoryIcon"></i>{" "}
+              <img src={Properties} alt="Properties-logo" className="categoriesLogo"/>
               <h6 className="mt-3">Properties</h6>
             </div>
           </LinkContainer>
           <LinkContainer to="/vehicles">
             <div className="col-md-2 col-4 text-center categoryItems pt-3 pb-3 w-80">
-              <i className="fas fa-motorcycle fa-2x categoryIcon"></i>{" "}
+            <img src={Vehicles} alt="vehicles-logo" className="categoriesLogo"/>
               <h6 className="mt-3">Vehicles</h6>
             </div>
           </LinkContainer>
           <LinkContainer to="/furnitures">
             <div className="col-md-2 col-4 text-center categoryItems pt-3 pb-3">
-              <i className="fas fa-couch fa-2x categoryIcon "></i>
+            <img src={Furnitures} alt="furnitures-logo" className="categoriesLogo"/>
               <h6 className="mt-3">Furnitures</h6>
             </div>
           </LinkContainer>
           <LinkContainer to="/appliances">
             <div className="col-md-2 col-4 text-center categoryItems pt-3 pb-3">
-              <i className="fas fa-blender-phone fa-2x categoryIcon"></i>
+            <img src={Applicences} alt="appliences-logo" className="categoriesLogo"/>
               <h6 className="mt-3"> Appliances</h6>
             </div>
           </LinkContainer>
           <LinkContainer to="/electronics">
             <div className="col-md-2 col-4 text-center categoryItems pt-3 pb-3">
-              <i className="fas fa-mobile fa-2x categoryIcon"></i>
+            <img src={Electronics} alt="electronics-logo" className="categoriesLogo"/>
               <h6 className="mt-3">Electronics</h6>
             </div>
           </LinkContainer>
           <LinkContainer to="/sports">
             <div className="col-md-2 col-4 text-center categoryItems pt-3 pb-3">
-              <i className="fas fa-dumbbell fa-2x categoryIcon"></i>{" "}
+            <img src={Sports} alt="sports-logo" className="categoriesLogo"/>
               <h6 className="mt-3">Sports</h6>
             </div>
           </LinkContainer>
--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = ({ location }) => {
   return (
     <Fragment>
       <h1 className="mt-5 pt-5 text-center" style={{color:'red'}}>
-      <i class="fas fa-exclamation-circle mr-3"></i>
+      <i className="fas fa-exclamation-circle mr-3"></i>
       
         
         Page Not Found
