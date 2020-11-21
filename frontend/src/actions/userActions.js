3c5894958f38467bdcb7201f4791c71cbd36c641
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-11-21 15:32:24 +0530

    profileDetails reducer and action created

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -6,7 +6,10 @@ import
  USER_LOGOUT,
  USER_REGISTER_REQUEST,
  USER_REGISTER_SUCCESS,
- USER_REGISTER_FAIL
+ USER_REGISTER_FAIL,
+ USER_DETAILS_REQUEST,
+ USER_DETAILS_SUCCESS,
+ USER_DETAILS_FAIL
 } from '../constants/userConstants'
  import axios from 'axios'
 
@@ -71,6 +74,34 @@ import
     } catch (error) {
         dispatch({
             type:USER_REGISTER_FAIL,
+            payload:error.response && error.response.data.message ? error.response.data.message
+            : error.message
+        })
+    }
+}
+ export const getUserDetails =(id) => async(dispatch,getState) =>{
+    try {
+        dispatch({
+            type:USER_DETAILS_REQUEST
+        })
+        const {userLogin:{userInfo}} = getState()
+        const config = {
+            headers:{
+                'Content-Type': 'application/json',
+                Authorization : `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.get(`/api/users/${id}`, config)
+
+        dispatch({
+            type:USER_DETAILS_SUCCESS,
+            payload:data
+        })
+        
+    } catch (error) {
+        dispatch({
+            type:USER_DETAILS_FAIL,
             payload:error.response && error.response.data.message ? error.response.data.message
             : error.message
         })
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,7 +6,10 @@ import
    USER_LOGOUT,
    USER_REGISTER_REQUEST,
    USER_REGISTER_SUCCESS,
-   USER_REGISTER_FAIL
+   USER_REGISTER_FAIL,
+   USER_DETAILS_REQUEST,
+   USER_DETAILS_SUCCESS,
+   USER_DETAILS_FAIL
 } 
 from '../constants/userConstants'
 
@@ -25,12 +28,12 @@ export const userLoginReducer =(state = {},action)=>{
     }
 }
 
-export const userRegisterReducer =(state = {},action)=>{
+export const userRegisterReducer =(state = {user:{}},action)=>{
     switch(action.type){
         case USER_REGISTER_REQUEST : 
-        return {loading:true}
+        return {...state,loading:true}
         case USER_REGISTER_SUCCESS:
-            return {loading:false,userInfo:action.payload}
+            return {loading:false,user:action.payload}
         case USER_REGISTER_FAIL:
             return {loading:false,error:action.payload}
             case USER_LOGOUT:
@@ -38,3 +41,18 @@ export const userRegisterReducer =(state = {},action)=>{
         default: return state
     }
 }
+
+export const userDetailsReducer =(state = {},action)=>{
+    switch(action.type){
+        case USER_DETAILS_REQUEST : 
+        return {loading:true}
+        case USER_DETAILS_SUCCESS:
+            return {loading:false,userInfo:action.payload}
+        case USER_DETAILS_FAIL:
+            return {loading:false,error:action.payload}
+            case USER_LOGOUT:
+                return {}        
+        default: return state
+    }
+}
+
