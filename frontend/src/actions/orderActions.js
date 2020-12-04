719cf694423a6034f32c62523da0c1e87aa77cd6
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-12-04 11:02:32 +0530

    fix

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -72,7 +72,7 @@ export const payOrder = (orderId,paymentResult)=> async (dispatch,getState)=>{
             Authorization : `Bearer ${userInfo.token}`
         }
     }
-       const {data}= await axios.get(`/api/orders/${orderId}/pay`,paymentResult,config)
+       const {data}= await axios.get(`/api/orders/${orderIgitd}/pay`,paymentResult,config)
        dispatch({
            type:ORDER_PAY_SUCCESS,
            payload:data
