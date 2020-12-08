b331b36abba5a4d36849c961db53fc27128b7dea
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-12-09 00:01:34 +0530

    userbyId and userUpdate route admin only

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -126,6 +126,45 @@ const deleteUser = asyncHandler(async(req,res)=>{
     }
 })
 
-export {authUser ,registerUser ,getUserProfile,updateUserProfile,getUsers,deleteUser} 
+// @desc get user by id
+// @route Get /api/users/:id
+// @access  private/admin
+const getUserById = asyncHandler(async(req,res)=>{
+
+  const user = await User.findById(req.params.id).select('-password')
+  if(user){
+    res.json(user)
+  }else{
+     res.status(404)
+     throw new Error('User Not Found')
+  }
+    
+})
+
+// @desc Update User
+// @route PUT /api/users/:id
+// @access  private/admin
+const updateUser= asyncHandler(async(req,res)=>{
+
+  const user = await User.findById(req.params.id)
+    if(user){
+       user.name = req.body.name || user.name,
+       user.email = req.body.email  || user.email
+       user.isAdmin = req.body.isAdmin
+       const updatedUser = await user.save()
+       res.json({
+        _id:updatedUser._id,
+        name:updatedUser.name,
+        email:updatedUser.email,
+        isAdmin:updatedUser.isAdmin,
+      })
+    }else{
+      res.status(404)
+      throw new Error('User not found')
+    }
+})
+
+
+export {authUser ,registerUser ,getUserProfile,updateUserProfile,getUsers,deleteUser,getUserById,updateUser} 
 
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const router = express.Router()
-import {authUser, deleteUser, getUserProfile,getUsers,registerUser,updateUserProfile} from '../controllers/userController.js'
+import {authUser, deleteUser, getUserById, getUserProfile,getUsers,registerUser,updateUser,updateUserProfile} from '../controllers/userController.js'
 import {admin, protect} from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser).get(protect,admin,getUsers)
 
-router.route('/:id').delete(protect,admin,deleteUser)
+router.route('/:id').delete(protect,admin,deleteUser).get(protect,admin,getUserById).put(protect,admin,updateUser)
 
 router.post('/login', authUser)
 
