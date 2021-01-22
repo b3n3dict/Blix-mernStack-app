98d33d4e072953e26ed1e592a786f6746a2cc8e7
georgejeswin <jeswinmyladoor@gmail.com>
2021-01-22 17:36:14 +0530

    payment form radio

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,47 +1,66 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Form, Button, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import { savePaymentMethod } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Radio from "@material-ui/core/Radio";
 
-import React,{useState} from 'react'
-import {useDispatch,useSelector} from 'react-redux'
-import {Form,Button,Col} from 'react-bootstrap'
-import FormContainer from '../components/FormContainer'
-import {savePaymentMethod} from '../actions/cartActions'
-import CheckoutSteps from '../components/CheckoutSteps'
-const PaymentScreen = ({history}) => {
-    const cart = useSelector(state => state.cart)
-    const {shippingAddress} = cart;
-     if(!shippingAddress) {
-         history.push('/shipping')
-     }
-    const [paymentMethod,setPaymentMethod] = useState('PayPal')
+const PaymentScreen = ({ history }) => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  if (!shippingAddress) {
+    history.push("/shipping");
+  }
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
 
-    const dispatch = useDispatch()
-    const submitHandler =(e)=>{
-        e.preventDefault()
-       dispatch(savePaymentMethod(paymentMethod))
-       history.push('/placeorder')
-      
-    }
-    return (
-        <FormContainer>
-            <CheckoutSteps step1 step2 step3/>
-            <h1>Payment</h1>
-            <Form onSubmit={submitHandler} >
-            <Form.Group>
-                <Form.Label as="legend">Select Method</Form.Label>
-            <Col>
-                <Form.Check type="radio" label="Paypal or Credit Card" id="PayPal"
-                 name="paymentMethod" value='PayPal' checked 
-                 onChange={(e)=> setPaymentMethod(e.target.value)}>
-                </Form.Check>
-                {/* <Form.Check type="radio" label="Stripe" id="Stripe"
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    history.push("/placeorder");
+  };
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>
+          <Col>
+            {/* <Form.Check
+              type="radio"
+              label="Paypal or Credit Card"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check> */}
+
+            <FormControlLabel
+              control={<Radio color="primary" />}
+              label="Paypal or Credit Card"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+
+            {/* <Form.Check type="radio" label="Stripe" id="Stripe"
                  name="paymentMethod" value='Stripe' 
                  onChange={(e)=> setPaymentMethod(e.target.value)}>
                 </Form.Check> */}
-            </Col>
-            </Form.Group>
-                <Button type="submit" variant="primary">Continue</Button>
-                </Form>
-        </FormContainer>
-    )
-}
+          </Col>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
 
-export default PaymentScreen
+export default PaymentScreen;
