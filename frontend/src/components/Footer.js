5aea028bbabe7002d15010ba238dc64eb9a6a870
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-01-24 19:35:53 +0530

    footer setup

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import "./Footer.css";
+import SendIcon from '@material-ui/icons/Send';
+import Message from "./Message";
 
 const FooterPage = () => {
+ const [email, setEmail] = useState(false)
+ const [value, setValue] = useState('')
+  const emailSubmit =(e)=> {
+    e.preventDefault()
+    setEmail(true)
+    setValue('')
+  }
+  setTimeout(()=>{
+      setEmail(false)
+  },8000)
   return (
-    <MDBFooter color="blue" className="font-small footer pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="6">
+
+    <MDBFooter color="blue" className="font-small footer pt-4 mt-4 neu__footer">
+    {email && <Message  severity="success">Email Sent. We will contact you with in two days</Message> } 
+      <MDBContainer fluid className="text-center text-md-left mt-5 ">
+        <MDBRow className="mdRow">
+          <MDBCol md="3">
             <h5 className="title">Footer Content</h5>
             <p>
               Here you can use rows and columns here to organize your footer
               content.
             </p>
           </MDBCol>
-          <MDBCol md="6">
+          
+          <MDBCol md="3">
+            <h5 className="title">Footer Content</h5>
+            <p>
+              Here you can use rows and columns here to organize your footer
+              content.
+            </p>
+          </MDBCol>
+          <MDBCol md="3">
+          <div class="input-group">
+    <form style={{display:"flex"}} onSubmit={emailSubmit} >
+      <input type="email" required  className="inFt"placeholder="Email Address" onChange={(e)=>setValue(e.target.value)} value={value}/>
+      <button className="ftb unit" type="submit" ><SendIcon/></button>
+    </form>
+    
+  </div>
+          </MDBCol>
+          <MDBCol md="3">
             <h5 className="title">Links</h5>
             <ul>
               <li className="list-unstyled">
@@ -31,6 +62,7 @@ const FooterPage = () => {
               </li>
             </ul>
           </MDBCol>
+       
         </MDBRow>
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
